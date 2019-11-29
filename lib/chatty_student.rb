01b611bc8ebc0c595b274cd56670b25930de3a70
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-29 14:41:21 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,8 +2,7 @@ require "pry"
 class ChattyStudent 
   
   def hello
-      puts "Hey there! I'm so excited to learn stuff.\nHoware you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
-      binding.pry
+     
   end
   
 end
